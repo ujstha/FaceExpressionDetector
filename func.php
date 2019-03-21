6097eee95dc69beb0cf99f069115065149899863
ujstha <ujstha27@gmail.com>
2019-03-21 17:22:45 +0200

    initialize app with function database

---
+++ func.php
@@ -0,0 +1,14 @@
+<?php
+	function makeconnection()
+	{
+		$cn=mysqli_connect("localhost","root","","webcam");
+		if(mysqli_connect_errno())
+		{
+			echo "failed to connect to mysqli:".mysqli_connect_error();
+		}
+		return $cn;
+	}
+
+	$cn=mysqli_connect("localhost","root","","webcam");
+
+?>
